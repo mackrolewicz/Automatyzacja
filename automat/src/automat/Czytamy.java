b8acdbf953e95d9741e6f0db3628608c050a9b50
mackrolewicz <mackrolewicz@gmail.com>
2019-05-27 11:57:10 +0200

    POprawiony z sortem babelkowym

--- automat/src/automat/Czytamy.java
+++ automat/src/automat/Czytamy.java
@@ -3,13 +3,9 @@ package automat;
 import java.awt.*;
 
 import java.io.*;
-
 import java.util.ArrayList;
-
 import java.util.regex.Matcher;
-
 import java.util.regex.Pattern;
-
 import java.util.*;
 
  
@@ -32,210 +28,107 @@ public class Czytamy{
 
              File file = new File("c:\\Users\\F93ME6X\\Desktop\\PROJEKTY-TESTY\\Automatyzacja\\Eclipse\\Czytamy.txt");
 
- 
-
              ArrayList<String> arr = new ArrayList<>();
-
              ArrayList<String> arr2 = new ArrayList<>();
-
              BufferedWriter writer = null;
-
              BufferedReader br = new BufferedReader(new FileReader(file));
-
- 
 
              File outputFile = new File("c:\\Users\\F93ME6X\\Desktop\\PROJEKTY-TESTY\\Automatyzacja\\Eclipse\\Piszemy.txt");
 
- 
-
              writer = new BufferedWriter(new FileWriter(outputFile));
 
- 
-
              String st;
-
              String zmienna;
-
              String rest;
-
- 
 
              while ((st = br.readLine()) != null) {
 
- 
-
                     arr.add(st);
-
  
-
              }
 
- 
-
              String zmienna2;
-
              String zera = "0;0";
-
              String data = "201812";
-
              /* String[] wart3 = new String[60]; */
-
              String zmienna3;
-
              int sum =0;
-
-            
-
- 
 
              for (int u = 0; u < arr.size(); u++) {
 
                     String[] lineData = arr.get(u).split(";");
 
- 
-
                     /*
-
                     * zmienna = arr.get(u).substring(18, 21); zmienna2 = arr.get(u).substring(9,
-
                     * 17);
-
                     */
-
                     zmienna3 = arr.get(u);
-
- 
-
                     String c1 = lineData[0];
-
                     String c2 = lineData[1];
-
                     String c3 = lineData[2];
-
                     String c4 = lineData[3];
 
- 
-
                     /*
-
                     * if (zmienna2 != data || zmienna == zera) {
-
                     * System.out.println(" cos nie tak"); continue; } else { arr2.add(zmienna3); }
-
                     */
-
- 
-
                     if ((c3 + ";" + c4).equals(zera) || !c2.substring(0,6).equals(data)) {
-
                            System.out.println(" cos nie tak");
-
                            continue;
-
+                           
                     } else {
 
                            int suma = Integer.parseInt(c3);
-
                            sum += suma;
-
-                          
-
-                           arr2.add(zmienna3);
-
+                   
+                          arr2.add(zmienna3);
                     }
 
              }
 
              System.out.println("To jest ta suma o tkora sie pytano:    " + sum);
-
-            
-
+          
              //sortowanie
-
             
-
              for(int j=0;j<arr2.size();j++){
-
-                   
-
-                                
-
-                   
-
+                                                                    
                                 for(int i=j+1;i<arr2.size();i++){
-
                    
-
                                 /*if(Integer.parseInt(arr2.get(i)) Integer.parseIntarr2.get(j))<0)*/
-
                                        
-
                                  if((arr2.get(i)).compareToIgnoreCase(arr2.get(j))<0){
-
-                   
-
-                                        
-
-                   
-
+                                                                              
                                         String t = arr2.get(j);
-
                                        
-
                                         arr2.set( j, arr2.get(i));
 
                                         arr2.set( i, t);
-
-                                              
-
+                                             
                                     }
-
                                    
-
- 
-
                                 }      
 
-                                writer.write("\n");
-
+                                 writer.write("\n");
                                  rest = arr2.get(j);
-
                                  writer.write(rest);
-
                                                                                       
-
                             System.out.println(arr2.get(j));
-
                    
-
                             }
 
              writer.close();
-
              //Collections.sort(arr2);
-
- 
-
              /*for (int l = 0; l < arr2.size(); l++) {
-
- 
 
                     writer.write("\n");
 
                     rest = arr2.get(l);
 
                     writer.write(rest);
-
  
-
              }
 
-             writer.close();*/
-
- 
-
-            
-
+             writer.close();*/           
              //Szukanie wyra¿eniem regularnym
 
             
@@ -257,7 +150,6 @@ public class Czytamy{
              String line = null;
 
              while ((line = br2.readLine()) != null) {
-
                     matcher.reset(line);
 
                     if (!matcher.find()) {
